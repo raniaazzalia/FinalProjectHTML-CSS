f3673872cbb91fafb384d0b24a6376ee909abef6
raniaazzalia <raniaazzalia09@gmail.com>
2023-05-23 15:26:24 +0800

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Rania Azzalia'; // TODO: replace this
+
+const studentId = 'FE5025959'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
